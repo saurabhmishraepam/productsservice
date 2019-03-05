f5ba484829dbc2a373a70aff2c5c8c4c718468b9
saurabhmishraepam <mishra.saurabh19@gmail.com>
2019-03-05 13:39:29 +0530

    week2 task

--- src/main/java/com/epam/productsreview/config/ApplicationConfig.java
+++ src/main/java/com/epam/productsreview/config/ApplicationConfig.java
@@ -1,13 +1,25 @@
 package com.epam.productsreview.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * Created by saurabh on 26/2/19.
  */
 @Configuration
 public class ApplicationConfig {
-
-
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
 
 }
