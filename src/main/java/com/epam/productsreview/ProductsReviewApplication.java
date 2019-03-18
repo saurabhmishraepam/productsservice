4f4f0e671b9a937e4e3be80ae2202f510c173aad
saurabh <mishra.saurabh19@gmail.com>
2019-03-19 00:42:14 +0530

    week 3 task

--- src/main/java/com/epam/productsreview/ProductsReviewApplication.java
+++ src/main/java/com/epam/productsreview/ProductsReviewApplication.java
@@ -2,8 +2,10 @@ package com.epam.productsreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductsReviewApplication {
 
 	public static void main(String[] args) {
