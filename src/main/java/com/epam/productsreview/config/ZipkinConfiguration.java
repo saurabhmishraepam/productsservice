3c9e53c0d0ee6db9be0fefe2506d42cb99e2330a
saurabhmishraepam <48027798+saurabhmishraepam@users.noreply.github.com>
2019-04-02 16:59:30 +0530

    Update ZipkinConfiguration.java

--- src/main/java/com/epam/productsreview/config/ZipkinConfiguration.java
+++ src/main/java/com/epam/productsreview/config/ZipkinConfiguration.java
@@ -1,11 +1,6 @@
 package com.epam.productsreview.config;
 
 import brave.sampler.Sampler;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.Span;
